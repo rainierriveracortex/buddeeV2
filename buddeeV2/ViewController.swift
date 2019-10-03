9fbfd8c268949a8268c36049574a63caea8d46b6
johnrainier.rivera <johnrainier.rivera@cortex.ph>
2019-10-03 13:29:19 +0800

    Initial commit

---
+++ buddeeV2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  buddeeV2
+//
+//  Created by jlrivera on 03/10/2019.
+//  Copyright © 2019 jlrivera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ buddeeV2Tests/buddeeV2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  buddeeV2Tests.swift
+//  buddeeV2Tests
+//
+//  Created by jlrivera on 03/10/2019.
+//  Copyright © 2019 jlrivera. All rights reserved.
+//
+
+import XCTest
+@testable import buddeeV2
+
+class buddeeV2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
